fb61ed304f5bddd718f4390c5482467052507006
ShaQay3Q <shaghay3gh@gmail.com>
2025-06-08 15:08:08 +0200

    first function in python

--- program.py
+++ program.py
@@ -1,25 +1,5 @@
-age = 39
-kid_age = 0
+def say_hi(name):
+    print ("Hi ", name)
 
-if age < 40:
-    condition = "young"
-    print ("You're ", condition, " babe!")
-
-if age == 40:
-    condition = "peaking"
-    print ("You're ", condition, " babe!")
-    kid_age = 5
-
-if age > 40:
-    condition = "great"
-    print ("You are ", condition, " babe!")
-
-print ("Your kid is ", kid_age, " old.")
-
-print ("___________________")
-
-for int in range(1,10):
-    print (int, int*int)
-
-for thing in [4, "456", "cat"]:
-    print (thing, thing*2)
+for name in ["Alex", "Dan", "Nima", "Narges", "Niaz"]:
+        say_hi(name) 
