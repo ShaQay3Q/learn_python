6d88108a2ac55d74d77ffdfebe11aeb5001727d7
ShaQay3Q <shaghay3gh@gmail.com>
2025-06-08 15:10:45 +0200

    first function in python

--- program.py
+++ program.py
@@ -2,4 +2,9 @@ def say_hi(name):
     print ("Hi ", name)
 
 for name in ["Alex", "Dan", "Nima", "Narges", "Niaz"]:
-        say_hi(name) 
+    say_hi(name)
+
+def add(a, b):
+    print ("a + b = ", a + b)
+
+add(11, 110)
